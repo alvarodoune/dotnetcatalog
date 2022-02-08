95ae85a89e6c4e60aee7e42f3f3c610d0c0be7a7
Alvaro Doune <alvarod@booster.software>
2022-02-08 17:42:35 -0300

    add secrets

--- Catalog/Settings/MongoDbSettings.cs
+++ Catalog/Settings/MongoDbSettings.cs
@@ -3,14 +3,18 @@ namespace Catalog.Settings
 {
     public class MongoDbSettings
     {
-        // docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db mongo
+        // docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=Pass#word1 mongo
         public string Host { get; set; }
 
         public string Port { get; set; }
 
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
         public string ConnectionString {
             get {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
